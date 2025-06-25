10811eedd711bfbb9f17f0895560390b31b12fc3
Vivek123 <vivek09122002@gmail.com>
2025-06-25 22:19:00 +0530

    Update all modified files

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,3 +120,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Redirect to home page after login
+LOGIN_REDIRECT_URL = '/'
